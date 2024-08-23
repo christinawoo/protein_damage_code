5c301b0880f3a118558ae96238e318ced6563525
Wenqing Xu <152343918+wenqingxu1997@users.noreply.github.com>
2024-08-23 13:27:19 -0400

    Update consensus_random50000_extract.py

--- random_10000_Asn/consensus_random50000_extract.py
+++ random_10000_Asn/consensus_random50000_extract.py
@@ -1,9 +1,9 @@
-#To extract 7-mer sequences around all N or Q in the human proteome
+# To extract 7-mer sequences around all N or Q in the human proteome
 
 import numpy as np
 import pandas as pd
 
-swissprot = pd.read_csv(r'/Users/wenqingxu/swissprot_human_proteome.csv')
+swissprot = pd.read_csv(r'')
 sequence = swissprot['Sequence'].tolist()
 print(len(sequence)) 
 
@@ -18,12 +18,12 @@ print(len(consensus))
 
 
 consensusallN = pd.DataFrame(consensus)
-consensusallN.to_csv('consensus_allN.csv', index=False)
+consensusallN.to_csv('', index=False)
 
-Ndata = pd.read_csv(r'/Users/wenqingxu/consensus_allN.csv')
+Ndata = pd.read_csv(r'')
 Nseq = Ndata['0'].tolist()
 
 import random
 randomN = random.sample(Nseq, 50000)
 dfN = pd.DataFrame(randomN)
-dfN.to_csv('randomN_50000.csv', index=False)
+dfN.to_csv('', index=False)
